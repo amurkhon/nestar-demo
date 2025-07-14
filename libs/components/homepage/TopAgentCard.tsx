524ff1cd08904ad7e123727a51990dbcd6ec438b
amurkhon <akramjonovamurxon02@gmail.com>
2025-07-14 18:31:23 +0900

    feat: publish homepage screen component

---
+++ libs/components/homepage/TopAgentCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { Stack } from '@mui/material';
+
+const TopAgentCard = () => {
+		return (
+			<Stack className="top-agent-card">
+				<img src={"/img/profile/girl.svg"} alt="" />
+
+				<strong>Martin</strong>
+				<span>Agent</span>
+			</Stack>
+		);
+	}
+
+export default TopAgentCard;
